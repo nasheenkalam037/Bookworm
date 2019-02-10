d61c4cd523c34f4ef622004e4bb92504ce5104d5
Syeda Noor Jaha Azim <snjazim@uwaterloo.ca>
2019-02-09 23:44:40 -0500

    Setup Pipelines

---
+++ src/web/gulpfile.js
@@ -0,0 +1,12 @@
+// gulpfile.js
+const gulp = require('gulp');
+
+gulp.task('lint-css', () => {
+  console.log('lint-css works!');
+});
+gulp.task('lint-js', () => {
+  console.log('lint-js works!');
+});
+gulp.task('test', () => {
+  console.log('test works!');
+});
