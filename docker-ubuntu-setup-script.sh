e3b9bc41af020fea8a2965315fc77b3d12b74ee5
Jonathan Shahen <jonathan.shahen@gmail.com>
2019-02-19 21:14:03 -0500

    switching to postgres 10, seeing if it works and what errors occur

--- docker-ubuntu-setup-script.sh
+++ docker-ubuntu-setup-script.sh
@@ -13,31 +13,31 @@ echo ==============================================
 export DEBIAN_FRONTEND=noninteractive
 #install tzdata package
 apt-get update && apt-get install -y tzdata
-
 apt-get update && apt-get install -y curl wget gnupg2
-
 echo 
 echo 
-echo ==============================================
-echo Installing Postgres 11
-echo ==============================================
-# INSTALL POSTGRES
-export RELEASE=$(cat /etc/os-release  | grep VERSION_CODENAME  | cut -d= -f2)
-echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg 11 | tee  /etc/apt/sources.list.d/pgdg.list
-
-cat /etc/apt/sources.list.d/pgdg.list
-wget --quiet https://www.postgresql.org/media/keys/ACCC4CF8.asc
-apt-key add ACCC4CF8.asc
-
-apt-get update
-
-sleep 2
-
-apt-cache show postgresql-11
-
-echo exit 0 > /usr/sbin/policy-rc.d
-
-apt-get update && apt-get install -y postgresql-11 postgresql-server-dev-11 libpq-dev
+if [[ -z "${POSTGRES_11}" ]]; then
+    echo ==============================================
+    echo Installing Postgres 10
+    echo ==============================================
+    echo exit 0 > /usr/sbin/policy-rc.d
+    apt-cache show postgresql-10
+    apt-get update && apt-get install -y postgresql-10 postgresql-server-dev-10 libpq-dev
+else
+    echo ==============================================
+    echo Installing Postgres 11
+    echo ==============================================
+    # INSTALL POSTGRES
+    export RELEASE=$(cat /etc/os-release  | grep VERSION_CODENAME  | cut -d= -f2)
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg 11 | tee  /etc/apt/sources.list.d/pgdg.list
+    cat /etc/apt/sources.list.d/pgdg.list
+    wget --quiet https://www.postgresql.org/media/keys/ACCC4CF8.asc
+    apt-key add ACCC4CF8.asc
+    apt-get update
+    apt-cache show postgresql-11
+    echo exit 0 > /usr/sbin/policy-rc.d
+    apt-get update && apt-get install -y postgresql-11 postgresql-server-dev-11 libpq-dev
+fi
 
 echo 
 echo 
