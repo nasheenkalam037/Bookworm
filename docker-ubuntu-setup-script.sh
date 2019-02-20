730d41796b9d8814f63ca32613175b378241a0d6
Jonathan Shahen <jonathan.shahen@gmail.com>
2019-02-20 14:44:07 -0500

    Testing install of webdriver and chromium browser

--- docker-ubuntu-setup-script.sh
+++ docker-ubuntu-setup-script.sh
@@ -45,7 +45,6 @@ echo
 echo ==============================================
 echo Installing Python3 and Pip3
 echo ==============================================
-# INSTALL NODEJS
 apt-get install -y python3-pip python3
 #
 #
@@ -54,7 +53,6 @@ echo
 echo ==============================================
 echo Installing Nodejs
 echo ==============================================
-# INSTALL NODEJS
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get update && apt-get install -y nodejs
 #
@@ -62,9 +60,48 @@ apt-get update && apt-get install -y nodejs
 echo 
 echo 
 echo ==============================================
+echo Installing WebDriver and Chromium
+echo ==============================================
+# Taken from https://www.blazemeter.com/blog/how-to-run-selenium-tests-in-docker
+# Use above when creating the docker image
+apt-get update && apt-get install -yq \
+    firefox-esr=52.6.0esr-1~deb9u1 \
+    chromium=62.0.3202.89-1~deb9u1 \
+    xvfb=2:1.19.2-1+deb9u2 \
+    xsel=1.2.0-2+b1 \
+    unzip=6.0-21 \
+    python-pytest=3.0.6-1 \
+    libgconf2-4=3.2.6-4+b1 \
+    libncurses5=6.0+20161126-1+deb9u2 \
+    libxml2-dev=2.9.4+dfsg1-2.2+deb9u2 \
+    libxslt-dev \
+    libz-dev \
+    xclip=0.12+svn84-4+b1
+# GeckoDriver v0.19.1
+wget -q "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz" -O /tmp/geckodriver.tgz \
+    && tar zxf /tmp/geckodriver.tgz -C /usr/bin/ \
+    && rm /tmp/geckodriver.tgz
+
+# chromeDriver v2.35
+wget -q "https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip" -O /tmp/chromedriver.zip \
+    && unzip /tmp/chromedriver.zip -d /usr/bin/ \
+    && rm /tmp/chromedriver.zip
+
+# xvfb - X server display
+ln -s /usr/bin/xvfb-chromium /usr/bin/google-chrome \
+    && chmod 777 /usr/bin/xvfb-chromium
+
+# create symlinks to chromedriver and geckodriver (to the PATH)
+ln -s /usr/bin/geckodriver /usr/bin/chromium-browser \
+    && chmod 777 /usr/bin/geckodriver \
+    && chmod 777 /usr/bin/chromium-browser
+#
+#
+echo 
+echo 
+echo ==============================================
 echo Testing Installed Versions of Software
 echo ==============================================
-# Check versions of software
 node --version
 python3 --version
 pip3 --version
