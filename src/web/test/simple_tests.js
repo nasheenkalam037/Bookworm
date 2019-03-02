ea6bf014fc67d084bbe3b2aafc2012fdc761ca2c
Jonathan Shahen <jonathan.shahen@gmail.com>
2019-03-02 17:45:40 -0500

    simple comment and moving test cases around

--- src/web/test/simple_tests.js
+++ src/web/test/simple_tests.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+
+// Reference to how to write tests: https://mochajs.org/
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
