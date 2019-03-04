8e872ca76dad9cdcdc8f49781dbf6de35be40162
n2kalam <n2kalam@uwaterloo.ca>
2019-03-03 23:55:12 -0500

    updating negative cases for authentication

--- src/web/test_case_scripts/test_signin.py
+++ src/web/test_case_scripts/test_signin.py
@@ -3,9 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+
+
 # keyword = "agile testing lisa crispin"
 driver = webdriver.Chrome()
-# driver = webdriver.Chrome('/src/web/driver/chromedriver')
+#driver = webdriver.Chrome('/home/nasheen/Documents/ECE651/chromedriver')
 driver.implicitly_wait(500)
 driver.get("http://127.0.0.1:3000/")
 
@@ -16,7 +18,7 @@ elem.click()
 driver.implicitly_wait(100)
 
 elem = driver.find_element_by_id("email")
-elem.send_keys("automation_test@gmail.com")
+elem.send_keys("automation_test23@gmail.com")
 
 elem = driver.find_element_by_id("password")
 elem.send_keys("bookworm123")
@@ -24,11 +26,29 @@ elem.send_keys("bookworm123")
 login=driver.find_element_by_id("login")
 login.click()
 
-driver.implicitly_wait(500)
+if len(driver.find_elements_by_class_name('error')) >0:
+    print ("Test Failed: Username and Password combination mismatch")
 
-if (driver.find_element_by_xpath(("//*[contains(text(), 'Hello Automation User')]"))):
-    print ("Test Passed")
+    elem = driver.find_element_by_id("email")
+    elem.send_keys("automation_test@gmail.com")
+
+    elem = driver.find_element_by_id("password")
+    elem.send_keys("bookworm123")
+
+    login = driver.find_element_by_id("login")
+    login.click()
+
+    if len(driver.find_elements_by_xpath("//*[contains(text(), 'Hello Automation User')]")) > 0:
+        #driver.implicitly_wait(200)
+        print ("Test Passed")
+    else:
+        print("User not found")
+
+
+# elif len(driver.find_elements_by_xpath("//*[contains(text(), 'An error occurred when trying to setup an account')]")) >0:
+#      print ("Test Failed: Username and Password combination mismatch")
+
 else:
     print ("Test Failed")
-driver.implicitly_wait(200)
+
 driver.quit()
--- src/web/test_case_scripts/test_signup.py
+++ src/web/test_case_scripts/test_signup.py
@@ -5,31 +5,55 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-# driver = webdriver.Chrome('/src/web/driver/chromedriver')
+#driver = webdriver.Chrome('/home/nasheen/Documents/ECE651/chromedriver')
 driver.implicitly_wait(500)
 driver.get("http://127.0.0.1:3000/account/signup")
 
 
 elem = driver.find_element_by_id("fullname")
-elem.send_keys("Test User01")
+elem.send_keys("Test User04")
 
 elem = driver.find_element_by_id("email")
-elem.send_keys("testuser@gmail.com")
+elem.send_keys("testuser4@gmail.com")
 
 elem = driver.find_element_by_id("password")
-elem.send_keys("usertest01")
+elem.send_keys("usertest05")
 
 elem = driver.find_element_by_id("confirmation_password")
-elem.send_keys("usertest01")
+elem.send_keys("usertest09")
 
 signup = driver.find_element_by_id("signup")
 signup.click()
 
 
-if (driver.find_element_by_xpath ("//*[contains(text(), 'Test User01')]")):
-    print ("Test Passed")
+if len(driver.find_elements_by_xpath ("//*[contains(., 'do not match')]"))>0:
+    print ("Invalid Input")
+    elem = driver.find_element_by_id("fullname")
+    elem.send_keys("Test User06")
+
+    elem = driver.find_element_by_id("email")
+    elem.send_keys("testuser6@gmail.com")
+
+    elem = driver.find_element_by_id("password")
+    elem.send_keys("usertest04")
+
+    elem = driver.find_element_by_id("confirmation_password")
+    elem.send_keys("usertest04")
+
+    signup = driver.find_element_by_id("signup")
+    signup.click()
+
+    if len(driver.find_elements_by_xpath("//*[contains(text(), 'Test User06')]")) > 0:
+        print ("Test Passed: User created")
+
+    elif len(driver.find_elements_by_xpath("//*[contains(.,'did you forget')]")) > 0:
+        print("User Already Exists")
+
+elif len(driver.find_elements_by_xpath ("//*[contains(., 'Duplicate Email found in table')]"))>0:
+    print("User Already Exists")
+
 else:
     print ("Test Failed")
 
-driver.implicitly_wait(200)
+#driver.implicitly_wait(200)
 driver.quit()
