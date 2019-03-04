9e69a2997a5494b9c035be9298a5cca3986cb4d0
Syeda Noor Jaha Azim <snjazim@uwaterloo.ca>
2019-03-04 11:22:26 -0500

    Added SEQUENCE query to sql

--- src/db/grant_tables_add_constraint.sql
+++ src/db/grant_tables_add_constraint.sql
@@ -1,7 +1,12 @@
--- TODO: add all CREATE SEQUENCE statements here
-
 
 --- Books
+
+CREATE SEQUENCE public."Books_book_id_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 ALTER TABLE ONLY public."Books"
     ADD CONSTRAINT "Books_pkey" PRIMARY KEY (book_id);
@@ -20,6 +25,13 @@ GRANT ALL ON TABLE public."Books" TO ece651_web;
 GRANT ALL ON TABLE public."Books" TO ece651_scraper;
 
 --- Author
+
+CREATE SEQUENCE public."Author_author_id_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 ALTER TABLE ONLY public."Author"
     ADD CONSTRAINT "Author_pkey" PRIMARY KEY (author_id);
@@ -53,6 +65,13 @@ GRANT ALL ON TABLE public."AuthorBooks" TO ece651_scraper;
 
 --- Categories
 
+CREATE SEQUENCE public."Categories_categories_id_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 ALTER TABLE ONLY public."Categories"
     ADD CONSTRAINT "Categories_pkey" PRIMARY KEY (categories_id);
 
@@ -85,6 +104,13 @@ GRANT ALL ON TABLE public."BookCategories" TO ece651_scraper;
 
 --- BookOfTheDay
 
+CREATE SEQUENCE public."BookOfTheDay_idBookOfTheDay_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 ALTER TABLE ONLY public."BookOfTheDay"
     ADD CONSTRAINT "BookOfTheDay_pkey" PRIMARY KEY ("idBookOfTheDay");
 
@@ -113,6 +139,13 @@ GRANT ALL ON TABLE public."AmazonDetails" TO ece651_scraper;
 
 --- Users
 
+CREATE SEQUENCE public."Users_user_id_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 ALTER TABLE ONLY public."Users"
     ADD CONSTRAINT "Users_pkey" PRIMARY KEY (user_id);
 
@@ -126,6 +159,13 @@ GRANT ALL ON TABLE public."Users" TO ece651_web;
 GRANT ALL ON TABLE public."Users" TO ece651_scraper;
 
 --- Reviews
+
+CREATE SEQUENCE public."Reviews_review_id_sequence"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 ALTER TABLE ONLY public."Reviews"
     ADD CONSTRAINT "Reviews_pkey" PRIMARY KEY (review_id);
@@ -142,3 +182,9 @@ ALTER TABLE ONLY public."Reviews"
 GRANT ALL ON TABLE public."Reviews" TO ece651_ml;
 GRANT ALL ON TABLE public."Reviews" TO ece651_web;
 GRANT ALL ON TABLE public."Reviews" TO ece651_scraper;
+
+--- BookDetails
+
+GRANT ALL ON TABLE public."BookDetails" TO ece651_ml;
+GRANT ALL ON TABLE public."BookDetails" TO ece651_web;
+GRANT ALL ON TABLE public."BookDetails" TO ece651_scraper;
